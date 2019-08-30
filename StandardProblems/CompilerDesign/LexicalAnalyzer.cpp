0061c56dac1eb20e6af10a8e539da6c1a215c7c4
AlokikPathak <alokik2009@mindfiresolutions.com>
2019-08-30 16:01:10 +0530

    implement basic analyzer

--- StandardProblems/CompilerDesign/LexicalAnalyzer.cpp
+++ StandardProblems/CompilerDesign/LexicalAnalyzer.cpp
@@ -14,7 +14,7 @@ using namespace std;
 short int isKeyword(char buffer[]) {
     // List of Keywords in CPP
     // Reference: 'https://en.cppreference.com/w/cpp/keyword'
-    char keywordsCPP[96][10] = {
+    char keywordsCPP[96][20] = {
         "alignas","alignof","and","and_eq","asm","atomic_cancel","atomic_commit","atomic_noexcept","auto",
         "bitand","bitor","bool","break","case","catch","char","char8_t","char16_t","char32_t","class","compl",
         "concept","const","consteval","constexpr","const_cast","continue","co_await","co_return","co_yield",
@@ -39,12 +39,12 @@ short int isKeyword(char buffer[]) {
  * @param: char 'ch'
  * @return: short int, returns
  */
-short int isOperator(char ch) {
+short int isOperator(char buffer[]) {
     // Basic set of arithmetic operators
-    char operatorsCPP[] = "+-*/%=";
+    char operatorsCPP[4][2] = { "+", "-", "*", "/" };
     // Linear Search
     for(int i=0; i<6; i++)
-        if(operatorsCPP[i] == ch)
+        if( strcmp(operatorsCPP[i], buffer) == 0)
             return 1;
     // if ch is not a operator in CPP language
     return 0;
@@ -52,7 +52,31 @@ short int isOperator(char ch) {
 
 
 int main() {
+    char ch, buffer[20];
+    // Taking 'program.txt' file as i/p
+    ifstream fin("program.txt");
+    int i, j=0;
+    // Checking if the sample program open successfully
+    if(!fin.is_open()) {
+        cout<<"Error while opening the file\n";
+        exit(0);
+    }
+    // Loop to iterate char by char in file
+    while(!fin.eof()){
+        ch = fin.get();
 
-
+        if((ch==' ' || ch=='\n') && j!=0) {
+            buffer[j] = '\0';
+            j=0;
+            // 1. If the string is Operator
+            if( isOperator(buffer) )
+                cout<<"\n"<<buffer<<" : Operator";
+            else if( isKeyword(buffer) )
+                cout<<"\n"<<buffer<<" : Keyword";
+            else
+                cout<<"\n"<<buffer<<" : Identifier";
+        }else
+            buffer[j++] = ch;
+    }
+    fin.close();
 }
-
